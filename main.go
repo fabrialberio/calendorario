1cd0ff90cba0aee1ca83feba98b9d79d35f7d156
fabrialberio <->
2025-10-31 00:43:20 +0100

    feat: Add database package

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"calendorario/pkg/database"
 	"calendorario/pkg/middleware"
 	"calendorario/views"
 	"embed"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/a-h/templ"
 
@@ -15,6 +19,9 @@ import (
 var publicFS embed.FS
 
 func main() {
+	db := createDatabase()
+	defer db.Close()
+
 	mux := http.NewServeMux()
 	setupRoutes(mux)
 
@@ -25,6 +32,30 @@ func main() {
 
 	log.Println("Server started on port 8080.")
 	log.Fatal(server.ListenAndServe())
+}
+
+func createDatabase() *database.DB {
+	dsn := fmt.Sprintf(
+		"host=%s port=5432 user=%s password=%s dbname=%s",
+		os.Getenv("POSTGRES_CONTAINER_NAME"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	db, err := database.New(dsn)
+	for n_retries := 0; err != nil; n_retries++ {
+		if n_retries == 5 {
+			log.Fatalf("Error creating database after %v retries: %v", n_retries, err)
+		}
+
+		time.Sleep(5 * time.Second)
+		log.Printf("Error creating database, retrying: %v", err)
+		db, err = database.New(dsn)
+	}
+	log.Println("Database created successfully.")
+
+	return db
 }
 
 func setupRoutes(mux *http.ServeMux) {

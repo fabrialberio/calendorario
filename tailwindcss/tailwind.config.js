2cc7f7a11e51767d670e3923f206f8da647aeb6f
fabrialberio <->
2025-11-03 11:38:49 +0100

    refactor: Go back to using `tailwind.config.js` because it works with autocomplete

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["../views/**/*.html"],
+    theme: {
+        colors: {
+            "primary": "#49672E",
+            "on-primary": "#FFFFFF",
+            "secondary": "#57624A",
+            "raised": "#e3e3e3",
+            "error": "#BA1A1A",
+            "view": "#F9FAEF",
+            "surface": "#DBE7C9",
+            "on-surface": "#1A1D16",
+            "on-surface-dim": "#44483E",
+            "outline": "#44483E",
+            "scrim": "#000000",
+        },
+    },
+    plugins: [],
+}

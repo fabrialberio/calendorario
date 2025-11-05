b28de39f2f9c0999dec640a345c66f76f7951535
fabrialberio <->
2025-11-05 13:53:53 +0100

    cleanup: Use camelCase

--- views/login_templ.go
+++ views/login_templ.go
@@ -8,7 +8,7 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func Login(wrong_username_password bool, session_expired bool) templ.Component {
+func Login(wrongUsernamePassword bool, sessionExpired bool) templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
@@ -41,12 +41,12 @@ func Login(wrong_username_password bool, session_expired bool) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		if wrong_username_password {
+		if wrongUsernamePassword {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<p class=\"text-error\">Username o password errati</p>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-		} else if session_expired {
+		} else if sessionExpired {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<p class=\"text-error\">Sessione scaduta</p>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err

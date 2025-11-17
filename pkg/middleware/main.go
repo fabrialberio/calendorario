78baa35289818acb3c02405c5fdd2580e6168404
fabrialberio <->
2025-11-17 14:25:02 +0100

    feat: Add `middleware.WithUserCheck`

--- pkg/middleware/main.go
+++ pkg/middleware/main.go
@@ -4,6 +4,7 @@ import (
 	"calendorario/pkg/auth"
 	"calendorario/pkg/database"
 	"calendorario/pkg/requestcontext"
+	"calendorario/views"
 
 	"log"
 	"net/http"
@@ -23,6 +24,26 @@ func WithContext(database *database.Queries, next http.Handler) http.Handler {
 		ctx := r.Context()
 		rc := requestcontext.NewContext(ctx, database, user, err)
 		r = r.WithContext(rc)
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+type UserCheckerFunc func(user *database.User) bool
+
+func WithUserCheck(checker UserCheckerFunc, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rc := requestcontext.FromRequest(r)
+		user, err := rc.AuthenticatedUser()
+		if err != nil {
+			http.Redirect(w, r, views.DestLogout, http.StatusSeeOther)
+			return
+		}
+
+		if !checker(user) {
+			http.Error(w, "Status 403 forbidden.", http.StatusForbidden)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
